abd68b597adfaf9f78d1804ece4832cc0fab7f92
Ananasovna <a.n.ushkova@yandex.ru>
2022-06-29 18:20:21 +0200

    feat: add 29.06.22 task solving

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 'use strict';
 
-const textarea = document.querySelector('textarea');
-const button = document.querySelector('button');
-const script = document.querySelector('script');
+const textarea = document.querySelector('.textarea');
+const button = document.querySelector('.button');
+const script = document.querySelector('.script');
+const checkbox = document.querySelector('.checkbox');
+const messages = [];
 
 function sendMessage() {
-  let message = document.createElement('p');
-  message.className = 'message';
-  message.innerHTML = textarea.value;
+  const message = createMessage(textarea.value);
   script.before(message);
+  clearTextarea();
 }
 
-function sendMessageByEnter(event) {
+function createMessage(innerText) {
+  const element = document.createElement('div');
+  element.className = 'message';
+  
+  const text = document.createElement('p');
+  text.innerHTML = innerText;
+
+  const button = document.createElement('button');
+  button.className = 'message__delete-button';
+  button.innerHTML = '&#128937';
+  button.addEventListener('click', () => console.log(`Вы хотите удалить сообщение № ${messages.findIndex(item => item == element)}`));
+  
+  element.append(text);
+  element.append(button);
+  messages.push(element);
+  
+  return element;
+}
+
+function handleKeyboardEvent(event) {
   if (event.code == 'Enter') {
     sendMessage();
   };
 }
 
+function clearTextarea() {
+  textarea.value = '';
+}
+
+function showText() {
+  if (checkbox.checked == false) {
+    console.log('красный');
+  } else {
+    console.log('зеленый');
+  }
+}
+
 button.addEventListener('click', sendMessage);
-document.addEventListener('keydown', sendMessageByEnter);
+document.addEventListener('keydown', handleKeyboardEvent);
+checkbox.addEventListener('click', showText);
